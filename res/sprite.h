1eeb35e67c9102a57e2ebc1b682350be41e43646
Mauricio <juanpablobouzas@gmail.com>
2019-12-08 23:29:45 -0400

    new skin system + a new skin

--- res/sprite.h
+++ res/sprite.h
@@ -2,6 +2,8 @@
 #define _RES_SPRITE_H_
 
 extern const SpriteDefinition maya_sprite;
+extern const SpriteDefinition nerdymaya_sprite;
+extern const SpriteDefinition ema_glasses;
 extern const SpriteDefinition pickup_sprites;
 
 #endif // _RES_SPRITE_H_
--- res/sprite.res
+++ res/sprite.res
@@ -1,2 +1,6 @@
 SPRITE maya_sprite "sprite/maya.png" 8 8 FAST 4
+SPRITE nerdymaya_sprite "sprite/nerdy_maya.png" 8 8 FAST 4
+
+SPRITE ema_glasses "sprite/ema_glasses.png" 4 2 NONE 1
+
 SPRITE pickup_sprites "sprite/pickups.png" 3 3 NONE 1
